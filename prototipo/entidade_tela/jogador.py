46d2e4c2ceb65c28c7b1338884565e069204b871
Davi Junkes <davimenegazjunkes@hotmail.com>
2023-05-19 10:45:35 -0300

    jogador modificado

--- prototipo/entidade_tela/jogador.py
+++ prototipo/entidade_tela/jogador.py
@@ -1,5 +1,25 @@
 from entidadeTela import EntidadeTela
 
 class Jogador(EntidadeTela):
-    def __init__(self):
-        pass
+    def __init__(self, pos_tela, dimensoes, arma):
+        super().__init__(pos_tela, dimensoes)
+        self.vida = 3
+        self.arma = arma
+        self.powerups = []
+
+    def atualizar(eventos: list):
+        for evento in eventos:
+            pass
+
+    def eventoColisao(self, outro):
+        if isinstance(outro, Inimigo):
+            self.vida -= 1
+
+        if isinstance(outro, SalaPortaCima):
+            pass
+        if isinstance(outro, SalaPortaBaixo):
+            pass
+        if isinstance(outro, SalaPortaEsquerda):
+            pass
+        if isinstance(outro, SalaPortaDireita):
+            pass
--- prototipo/sala_porta.py
+++ prototipo/sala_porta.py
@@ -1,6 +1,6 @@
 from abc import ABC
 from entidade.sala import Sala
-from entidade_tela.Porta import Porta
+from entidade_tela.porta import Porta
 
 
 class SalaPorta(ABC):
