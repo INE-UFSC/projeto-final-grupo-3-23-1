781b67f66b71656babd7c6f8c9a9d4a807b236e6
RitaLB <119005903+RitaLB@users.noreply.github.com>
2023-05-21 20:49:23 -0300

    adicionei classe de gerenciamento de eventos

--- prototipo/mapa_jogo/inimigo.py
+++ prototipo/mapa_jogo/inimigo.py
@@ -1,15 +1,22 @@
 from basico.entidadeTela import EntidadeTela
-
-
+from basico.evento import Evento
 class Inimigo(EntidadeTela):
-    def __init__(self):
-        self.__dano = float
-        self.__velocidade = float
+
+    def __init__(self,tela, pos_tela, dimensoes, desenhavel, dano, velocidade ):
+        super().__init__(tela, pos_tela, dimensoes, desenhavel)
+        self.pos_atual = super.pos_tela
+        self.__dano = dano
+        self.__velocidade = velocidade
+        #inicializando classe evento:
+        self.__gerenciador_eventos = Evento()
 
     @property
     def dano(self):
         return self.__dano
     
+    def eventoColisão(self):
+        self.__gerenciador_eventos.verificarColisão(self, tiros)
+
 
 
 
