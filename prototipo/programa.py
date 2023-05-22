ee2a7112a2169d70847175517349a0f0a589217d
Felipe Elton <felipe.elton.ps@gmail.com>
2023-05-22 12:02:38 -0300

    Adicionado esboco de programa.py. Nao funciona ainda

---
+++ prototipo/programa.py
@@ -0,0 +1,37 @@
+import pygame as pg
+from pygame.locals import *
+
+from basico.entidadeTela import EntidadeTela, DesenhavelRetangulo
+from basico.evento import EventoPygame
+from jogador.jogador import Jogador
+from mapa_tela.mapa_tela import MapaTela
+
+pg.init()
+tela = pg.display.set_mode((500, 400))
+
+cor_fundo = (0, 0, 0)
+
+jogador = Jogador(tela)
+mapa_jogo = MapaJogo(tela)
+
+while True:
+    eventos = []
+    for evento in pg.event.get():
+        if evento.type == QUIT:
+            pg.quit()
+            exit()
+        if evento.type == pg.KEYDOWN:
+            eventos.append(EventoPygame(evento))
+
+    jogador.atualizar(eventos)
+    mapa_jogo.atualizar(eventos)
+            
+    EntidadeTela.sistema_colisao.checarColisoes()
+
+    tela.fill(cor_fundo)
+    teste_jogador.desenhar()
+    teste_inimigo.desenhar()
+
+    pg.display.update()
+    pg.time.delay(int(1000/60))
+
