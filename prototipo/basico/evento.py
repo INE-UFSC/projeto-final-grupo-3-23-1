9028b3b976657cd4af0a8aab86fb9a1aa98e85a7
Felipe Elton <felipe.elton.ps@gmail.com>
2023-05-21 23:31:32 -0300

    evento.py retornado ao estado anterior

--- prototipo/basico/evento.py
+++ prototipo/basico/evento.py
@@ -1,41 +1,7 @@
 from abc import ABC
-import pygame
+
 class Evento(ABC):
-    # lembrar que no pygame eventos são coletados de forma geral, não estão ligados a uma entidade.
-    def __init__(self,  jogo):
-        # para poder acessar todos os dados jo jogo, precisa ter o jogo como atributo
-        self.__jogo = jogo
-        #verifica eventos coletados pelo pygame no frame:
-        self.__eventos = pygame.event.get()
-        self.processar_eventos(self.__eventos)
-
-    #processa os eventos coletados:
-    def processar_eventos(eventos):
-        for event in eventos:
-            if event.type == pygame.KEYDOWN:
-                ...
-
-            #interar com dmais tipos de eventos
-    @classmethod
-    def verificarColisão(cls, entidade_eu, tipo_entidade):
-        instancia = cls('Verificar_colisão')
-        instancia.verificarColisão_2(entidade_eu, tipo_entidade)
-
-    def verificarColisão_2(self, entidade_eu, atributo_entidade_outro):
-        
-        #pode verificar colisão entre tipos de objetos específicos
-        #acessa objeto ou lista de objetos específica da sala a ser analizada a colisão:
-        atributo_entidade_outro = self.__jogo.mapa_jogo.sala_atual.atributo_entidade_outro
-        #se for uma lista de objetos (por exemplos, vários inimigos, ou vários tiros), vai verificar a colisão com cada um:
-        if atributo_entidade_outro.type == list:
-            for entidade in  atributo_entidade_outro: 
-                self.colisao(entidade_eu, entidade)
-        else: self.colisao(entidade_eu,  atributo_entidade_outro)
-
-    #verifica se duas entidades colidiram:
-    def colisao(entidade_eu, entidade_outro):
-        return entidade_eu.colliderect(entidade_outro)
-            
+    pass
 
 class EventoColisao(Evento):
     def __init__(self, outro):
