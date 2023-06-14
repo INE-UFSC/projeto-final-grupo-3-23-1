e73c8bee137d718d504b05e4eee7c9e99c6c9a97
Davi Junkes <davimenegazjunkes@hotmail.com>
2023-06-14 14:14:40 -0300

    movimentacao jogador certo

--- prototipo/jogo/jogador/jogador.py
+++ prototipo/jogo/jogador/jogador.py
@@ -1,4 +1,5 @@
 import pygame as pg
+from math import sin, cos, radians
 
 from basico.entidade_tela import EntidadeTela
 from basico.evento import *
@@ -42,34 +43,36 @@ class Jogador(EntidadeTela):
                 elif self.desenhavel.cor == (255, 255, 255):
                     self.desenhavel.cor = (0, 255, 0) 
                 self.last_tick = pg.time.get_ticks() 
-
+        
+        apertadas = []
+        moveu = False
         for evento in eventos:
             if isinstance(evento, EventoApertouTecla):
                 if evento.tecla == pg.K_k:
                     self.atirar(self.__powerups)
 
             if isinstance(evento, EventoTeclaApertada):
-                nova_pos = list(self.pos_tela)
+                if evento.tecla == pg.K_w or evento.tecla == pg.K_s or evento.tecla == pg.K_a or evento.tecla == pg.K_d:
+                    apertadas.append(evento.tecla)
+                    moveu = True
 
-                if evento.tecla == pg.K_w:
-                    if nova_pos[1] > self.dimensoes[1]/2:
-                        nova_pos[1] -= 5
+                if pg.K_w in apertadas:
                     self.__direcao = 270
-                if evento.tecla == pg.K_s:
-                    if nova_pos[1] < self.tela.get_height()-self.dimensoes[1]/2:
-                        nova_pos[1] += 5
+                if pg.K_s in apertadas:
                     self.__direcao = 90
-                if evento.tecla == pg.K_a:
-                    if nova_pos[0] > self.dimensoes[0]/2:
-                        nova_pos[0] -= 5
+                if pg.K_a in apertadas:
                     self.__direcao = 180
-                if evento.tecla == pg.K_d:
-                    if nova_pos[0] < self.tela.get_width()-self.dimensoes[0]/2:
-                        nova_pos[0] += 5 
+                if pg.K_d in apertadas:
                     self.__direcao = 0
+                if pg.K_w in apertadas and pg.K_a in apertadas:
+                    self.__direcao = 225
+                if pg.K_w in apertadas and pg.K_d in apertadas:
+                    self.__direcao = 315
+                if pg.K_s in apertadas and pg.K_a in apertadas:
+                    self.__direcao = 135
+                if pg.K_s in apertadas and pg.K_d in apertadas:
+                    self.__direcao = 45
 
-                self.pos_tela = tuple(nova_pos)
-                
 
             if isinstance(evento, EventoColisao) \
                     and evento.possui(self):
@@ -95,7 +98,16 @@ class Jogador(EntidadeTela):
                             self.pos_tela = (sala_porta.dimensoes[0]+self.dimensoes[0]/2, self.pos_tela[1])
                         elif isinstance(sala_porta, SalaPortaEsquerda):
                             self.pos_tela = (self.tela.get_width()-(sala_porta.dimensoes[0]+self.dimensoes[0]/2), self.pos_tela[1])
-
+        if moveu:
+            print(self.__direcao) 
+                
+            nova_pos = list(self.pos_tela)
+            if nova_pos[0] < self.tela.get_width()-self.dimensoes[0]/2 or nova_pos[0] > self.dimensoes[0]/2:
+                nova_pos[0] += 5 * cos(radians(self.__direcao))
+            if nova_pos[1] < self.tela.get_height()-self.dimensoes[1]/2 or nova_pos[1] > self.dimensoes[1]/2:
+                nova_pos[1] += 5 * sin(radians(self.__direcao))
+            self.pos_tela = tuple(nova_pos)
+                
         tiros_rem = []
         for t in self.__tiros:
             if not t.ativo:
