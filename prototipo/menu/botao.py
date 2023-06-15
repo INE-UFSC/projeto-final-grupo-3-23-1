543365a612ed0df09d89883e7b05fc8322eb1424
Davi Junkes <davimenegazjunkes@hotmail.com>
2023-06-15 13:18:15 -0300

    botao get/set

--- prototipo/menu/botao.py
+++ prototipo/menu/botao.py
@@ -6,23 +6,11 @@ import pygame as pg
 class Botao(EntidadeTela):
     def __init__(self, tela, pos_tela, dimensoes, desenhavel, texto, tamanho_fonte = 75/1080, fonte = "Comic Sans MS"):
         super().__init__(tela, pos_tela, dimensoes, desenhavel)
-        self.tela = tela
-        self.pos_tela = pos_tela
-        self.dimensoes = dimensoes
-        self.desenhavel = desenhavel
-        self.texto = texto
 
-        self.apertou = False
-    
-        self.font = pg.font.SysFont(fonte, int(tamanho_fonte * self.tela.get_height()))
+        self.__texto = texto
+        self.__fonte = pg.font.SysFont(fonte, int(tamanho_fonte * self.tela.get_height()))
 
-    def desenhar(self):
-        self.desenhavel.desenhar(self.pos_tela, self.dimensoes)
-
-        text_width, text_height = self.font.size(self.texto)
-#        print(text_width, text_height)
-        self.tela.blit(self.font.render(self.texto, False, (51, 0, 0)),
-                        (self.pos_tela[0] - text_width/2, self.pos_tela[1] - text_height/2))
+        self.__apertou = False
 
     def atualizar(self, eventos):
         for evento in eventos:
@@ -32,6 +20,29 @@ class Botao(EntidadeTela):
                 if rect.collidepoint(evento.pos_mouse): 
                     self.apertou = True
 
+    def desenhar(self):
+        self.desenhavel.desenhar(self.pos_tela, self.dimensoes)
+
+        text_width, text_height = self.fonte.size(self.texto)
+
+        self.tela.blit(self.fonte.render(self.texto, False, (51, 0, 0)),
+                        (self.pos_tela[0] - text_width/2, self.pos_tela[1] - text_height/2))
+
     def resetApertou(self):
         self.apertou = False
 
+    @property
+    def texto(self):
+        return self.__texto
+
+    @property
+    def fonte(self):
+        return self.__fonte
+
+    @property
+    def apertou(self):
+        return self.__apertou
+    
+    @apertou.setter
+    def apertou(self, bool):
+        self.__apertou = bool
